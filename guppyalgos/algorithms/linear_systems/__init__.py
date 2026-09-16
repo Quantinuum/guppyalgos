e695e5d38a08eb39014d2727f87f5ce24e684d9b
Walden <walden.killick@quantinuum.com>
2026-09-16 09:27:50 +0100

    chore: exclude irrelevant imports

--- guppyalgos/algorithms/linear_systems/__init__.py
+++ guppyalgos/algorithms/linear_systems/__init__.py
@@ -1,15 +1,5 @@
 """Quantum algorithms for solving linear systems of equations."""
 
-from .hhl import (
-    eigenvalue_inversion_angles,
-    hhl,
-    hhl_conditional_rotation,
-    hhl_power_oracles,
-)
+from .hhl import hhl
 
-__all__ = [
-    "eigenvalue_inversion_angles",
-    "hhl",
-    "hhl_conditional_rotation",
-    "hhl_power_oracles",
-]
+__all__ = ["hhl"]
